b11959a4a35a16a520c77a22cca015d52ef3fa20
Raphael Freitas <raphael.soarescivil@gmail.com>
2023-01-11 20:48:39 -0300

    feat: adicionado o SVG junto ao nome do header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import { HiOutlineUser } from 'react-icons/hi';
 
 function Header() {
   const USERNAME = 'João';
   const WELLCOMEPHRASE = `Olá, ${USERNAME}`;
   return (
     <div className="w-full">
-      <span
-        className="flex justify-end m-1 font-bold"
-      >
-        {WELLCOMEPHRASE}
-      </span>
+      <div className="flex justify-end p-2">
+        <span
+          className="m-1 font-bold"
+        >
+          {WELLCOMEPHRASE}
+        </span>
+        <span className="flex items-center">
+          <HiOutlineUser
+            className="bg-auto h-8 w-8"
+          />
+        </span>
+      </div>
       <nav
         className="bg-sharenergy-green text-white font-sans
           font-bold flex justify-evenly items-center h-11"
