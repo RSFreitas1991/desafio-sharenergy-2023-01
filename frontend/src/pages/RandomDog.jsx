e5ec277398daa771a38c6cfe77287b3a0f2a857b
Raphael Freitas <raphael.soarescivil@gmail.com>
2023-01-13 16:42:00 -0300

    fix: Modificado CSS da RandomDog Page

--- frontend/src/pages/RandomDog.jsx
+++ frontend/src/pages/RandomDog.jsx
@@ -20,14 +20,23 @@ function RandomDog() {
   }, []);
 
   return (
-    <div>
+    <div className="w-screen h-screen">
       <Header />
-      <div className="min-h-fit max-h-screen flex justify-center mt-2">
+      <div
+        className="min-h-fit max-h-screen flex justify-center
+        mt-2 md:w-screen md:h-screen md:items-center"
+      >
         <div
           className="bg-sharenergy-green rounded-md
-          w-full h-1/3 flex flex-col items-center justify-center md:flex-wrap md:max-w-sm"
+          w-full h-1/3 flex flex-col items-center justify-center
+          md:flex md:flex-col md:w-fit md:h-fit"
         >
-          <img id="dogIMG" alt="Random Dog Img" src={ dogLink } className="" />
+          <img
+            id="dogIMG"
+            alt="Random Dog Img"
+            src={ dogLink }
+            className="p-4 max-h-72"
+          />
           <fieldset className="flex flex-col">
             <button
               type="button"
